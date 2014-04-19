bb3dc6fb850502a0fb48ca4ab5c261d645ae2191
HJeon139 <gamerhj@gmail.com>
2014-04-19 10:19:44 -0400

    overhaul

---
+++ entities/entities/ph_prop/shared.lua
@@ -0,0 +1,13 @@
+--
+-- ph_prop/shared.lua
+-- Prop Hunt
+--	
+-- Created by Andrew Theis on 2013-03-09.
+-- Copyright (c) 2010-2013 Andrew Theis. All rights reserved.
+--
+ 
+
+-- Entity information.
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+
